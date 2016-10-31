8d7e298b7f22d0c858f5d71b5c8461a632aa5797
bilalasd <bilal2206@gmail.com>
2016-10-31 14:37:26 -0500

    -Updated the following algorithm.

---
+++ RobotFollowerWPF2/RobotFollowerWPF2/Helper Classes/Command.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotFollowerWPF2
+{
+    public class Command
+    {
+        public Command(actions initialAction, double initialMagnitude, double initialAngle, double initialRotation)
+        {
+            action = initialAction;
+            magnitude = initialMagnitude;
+            angle = initialAngle;
+            rotation = initialRotation;
+        }
+
+        public Command(actions initialAction)
+        {
+            action = initialAction;
+            magnitude = 0;
+            angle = 0;
+        }
+
+        public enum actions
+        {
+            FORWARD,
+            BACK,
+            TURN_LEFT,
+            TURN_RIGHT,
+            STOP
+        };
+
+        public double magnitude = 0;
+        public double angle = 0;
+        public double rotation = 0;
+        public actions action;
+
+
+    }
+}
---
+++ RobotFollowerWPF2/RobotFollowerWPF2/Helper Classes/Extension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace RobotFollowerWPF2
+{
+    public static class Extension
+    {
+        public static void SynchronizedInvoke(this ISynchronizeInvoke sync, Action action)
+        {
+            // If the invoke is not required, then invoke here and get out.
+            if (!sync.InvokeRequired)
+            {
+                // Execute action.
+                action();
+
+                // Get out.
+                return;
+            }
+
+            // Marshal to the required context.
+            sync.Invoke(action, new object[] { });
+        }
+        public static T[] Slice<T>(this T[] source, int index, int length)
+        {
+            T[] slice = new T[length];
+            Array.Copy(source, index, slice, 0, length);
+            return slice;
+        }
+
+        public static bool ControlInvokeRequired(System.Windows.Controls.Control c, Action a)
+        {
+            if (c.Dispatcher.CheckAccess())
+            {
+                c.Dispatcher.BeginInvoke((Action)(delegate { a(); }));
+            }
+            else return false;
+
+            return true;
+        }
+
+
+    }
+}
